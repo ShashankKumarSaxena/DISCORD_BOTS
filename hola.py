af77ec90288cfbdb0f59b6da3ec2277f0c529fe7
Mercy <dubeykrish012@gmail.com>
2021-02-07 11:03:59 +0530

    Added I am ready

---
+++ hola.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+import json
+bot = commands.Bot(command_prefix='m!', case_insensitive=True)
+
+with open("./config.json", 'r') as configjsonfile:
+    configData = json.load(configjsonfile)
+    TOKEN = configData["DISCORD_TOKEN"]
+
+@bot.event
+async def on_ready():
+    print('I am ready!')
